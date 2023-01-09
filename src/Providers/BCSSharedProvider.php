2af6d1bf18e48df958473873ef1bb413d60eaeb7
Toby Allen <toby@cookingisfun.ie>
2023-01-09 15:44:56 +0000

    fix dir

--- src/Providers/BCSSharedProvider.php
+++ src/Providers/BCSSharedProvider.php
@@ -23,6 +23,6 @@ class BCSSharedProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 }
